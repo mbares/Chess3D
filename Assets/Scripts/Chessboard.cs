cf679727bf9a5e6395642673cbf8672a6c501bc3
Mateo Bares <mateo.bares@voidmain.studio>
2018-12-03 18:36:45 +0100

    Chessboard implementation
    Implemented chessboard model with chessboard data SO that will contain board state information.

---
+++ Assets/Scripts/Chessboard.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Chessboard : ScriptableObject
+{
+    [System.Serializable]
+    public struct PiecePosition
+    {
+        public IChessPiece chessPiece;
+        public Vector2 position;
+    }
+
+    [HideInInspector]
+    public List<PiecePosition> piecePositions = new List<PiecePosition>();
+}
